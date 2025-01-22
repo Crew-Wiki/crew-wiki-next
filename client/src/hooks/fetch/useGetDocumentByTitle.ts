71af71a848644320a596927a6aa94c1c9ca09d51
김진호 <rlawlsgh1227@gmail.com>
2025-01-22 10:01:05 +0900

    fix: useCallback으로 계속 초기화되는 현상 제거

--- client/src/hooks/fetch/useGetDocumentByTitle.ts
+++ client/src/hooks/fetch/useGetDocumentByTitle.ts
@@ -3,9 +3,11 @@
 import {getDocumentByTitle} from '@api/document';
 import {useFetch} from '@hooks/useFetch';
 import {WikiDocument} from '@type/Document.type';
+import {useCallback} from 'react';
 
 export const useGetDocumentByTitle = (title: string) => {
-  const {data} = useFetch<WikiDocument>(() => getDocumentByTitle(title));
+  const getData = useCallback(() => getDocumentByTitle(title), [title]);
+  const {data} = useFetch<WikiDocument>(getData);
 
   return {
     document: data,
