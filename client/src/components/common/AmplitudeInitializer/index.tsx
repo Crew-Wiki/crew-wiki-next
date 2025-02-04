db70e21318b72f7dd62ebba4a0b2c67dd8ba97a4
김진호 <rlawlsgh1227@gmail.com>
2025-02-04 10:17:45 +0900

    feat: 필요하지 않은 autocapture 해제

--- client/src/components/common/AmplitudeInitializer/index.tsx
+++ client/src/components/common/AmplitudeInitializer/index.tsx
@@ -5,7 +5,14 @@ import {init} from '@amplitude/analytics-browser';
 
 export const AmplitudeInitializer = () => {
   useEffect(() => {
-    init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY);
+    init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY, {
+      autocapture: {
+        elementInteractions: false,
+        fileDownloads: false,
+        sessions: false,
+        formInteractions: false,
+      },
+    });
   }, []);
 
   return null;
