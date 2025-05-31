b33171648b51a196d43dfe4142d3ea609bb15f65
ShinjungOh <tlswjd9260@gmail.com>
2025-06-01 03:46:36 +0900

    feat: PopularDocumentList 컴포넌트 구현

---
+++ client/src/components/document/Popular/PopularDocumentList.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import {PopularDocument} from '@type/Document.type';
+import PopularDocumentItem from './PopularDocumentItem';
+
+interface PopularDocumentListProps {
+  documents: PopularDocument[];
+  sortType: 'views' | 'edits';
+  startRank?: number;
+}
+
+export default function PopularDocumentList({documents, sortType, startRank = 4}: PopularDocumentListProps) {
+  if (documents.length === 0) {
+    return (
+      <div className="bg-gray-50 border border-gray-200 rounded-lg p-8 text-center text-gray-500">
+        등록된 문서가 없습니다.
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      {documents.map((document, index) => (
+        <PopularDocumentItem
+          key={document.title}
+          document={document}
+          rank={startRank + index}
+          sortType={sortType}
+        />
+      ))}
+    </div>
+  );
+}
