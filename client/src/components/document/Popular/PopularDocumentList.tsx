48cdcddc8cbe3ff3f394ef454a971c2cb41c2cb3
ShinjungOh <tlswjd9260@gmail.com>
2025-06-01 04:05:18 +0900

    refactor: PopularDocumentList 조건부 렌더링 방식 변경

--- client/src/components/document/Popular/PopularDocumentList.tsx
+++ client/src/components/document/Popular/PopularDocumentList.tsx
@@ -10,20 +10,27 @@ interface PopularDocumentListProps {
 }
 
 const PopularDocumentList = ({documents, sortType, startRank = 4}: PopularDocumentListProps) => {
-  if (documents.length === 0) {
-    return (
-      <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 text-center text-gray-500">
-        등록된 문서가 없습니다.
-      </div>
-    );
-  }
+  const isDocumentEmpty = documents.length === 0;
 
   return (
-    <div className="flex flex-col gap-2">
-      {documents.map((document, index) => (
-        <PopularDocumentItem key={document.title} document={document} rank={startRank + index} sortType={sortType} />
-      ))}
-    </div>
+    <>
+      {isDocumentEmpty ? (
+        <div className="rounded-lg border border-gray-200 bg-gray-50 p-8 text-center text-gray-500">
+          등록된 문서가 없습니다.
+        </div>
+      ) : (
+        <div className="flex flex-col gap-2">
+          {documents.map((document, index) => (
+            <PopularDocumentItem
+              key={document.title}
+              document={document}
+              rank={startRank + index}
+              sortType={sortType}
+            />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
