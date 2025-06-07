88cc9fb18b9c6b7e89f8dd79debfc1ade7140fef
김진호 <rlawlsgh1227@gmail.com>
2025-06-07 14:56:21 +0900

    style: 불필요한 ! 제거

--- client/src/utils/processImageHtml.ts
+++ client/src/utils/processImageHtml.ts
@@ -1,6 +1,6 @@
 export function processImageHtml(html: string): string {
-  const s3Domain = process.env.NEXT_PUBLIC_IMAGE_S3_DOMAIN!;
-  const cloudfrontDomain = process.env.NEXT_PUBLIC_IMAGE_CLOUDFRONT_DOMAIN!;
+  const s3Domain = process.env.NEXT_PUBLIC_IMAGE_S3_DOMAIN;
+  const cloudfrontDomain = process.env.NEXT_PUBLIC_IMAGE_CLOUDFRONT_DOMAIN;
 
   return html.replace(/<img\s+([^>]*?)src="([^"]+)"([^>]*?)>/g, (match, before, src, after) => {
     try {
