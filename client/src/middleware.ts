b52d99d2b26274ea599b31f2e30fea3cfac82773
ShinjungOh <tlswjd9260@gmail.com>
2025-07-20 01:02:02 +0900

    feat: 어드민 사이드바 컴포넌트 구현

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -5,6 +5,7 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   const {pathname} = new URL(request.url);
 
+  // TODO: 개발 완료 후 주석 해제
   // if (pathname === '/admin/login' && token) {
   //   return NextResponse.redirect(new URL('/admin/documents', request.url));
   // }
