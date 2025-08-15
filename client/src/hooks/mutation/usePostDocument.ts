744cdac77e838a52e6a4ae42a4ebdd78cf3e7c12
bmj0053 <bmj05378@gmail.com>
2025-08-15 23:04:23 +0900

    feat: 문서 생성 및 업데이트 시 Trie에 제목과 UUID 추가 및 업데이트 기능 구현

--- client/src/hooks/mutation/usePostDocument.ts
+++ client/src/hooks/mutation/usePostDocument.ts
@@ -6,15 +6,18 @@ import {PostDocumentContent, WikiDocument} from '@type/Document.type';
 import {useRouter} from 'next/navigation';
 import useAmplitude from '@hooks/useAmplitude';
 import {postDocumentClient} from '@apis/client/document';
+import {useTrie} from '@store/trie';
 
 export const usePostDocument = () => {
   const router = useRouter();
+  const trie = useTrie(state => state.trie);
   const {trackDocumentCreate} = useAmplitude();
 
   const {mutate, isPending} = useMutation<PostDocumentContent, WikiDocument>({
     mutationFn: postDocumentClient,
     onSuccess: document => {
       trackDocumentCreate(document.title, document.documentUUID);
+      trie.insert(document.title, document.documentUUID);
       router.push(`${URLS.wiki}/${document.documentUUID}`);
       router.refresh();
     },
--- client/src/hooks/mutation/usePutDocument.ts
+++ client/src/hooks/mutation/usePutDocument.ts
@@ -6,15 +6,19 @@ import {PostDocumentContent, WikiDocument} from '@type/Document.type';
 import {useRouter} from 'next/navigation';
 import useAmplitude from '@hooks/useAmplitude';
 import {putDocumentClient} from '@apis/client/document';
+import {useTrie} from '@store/trie';
 
 export const usePutDocument = () => {
   const router = useRouter();
+  const trie = useTrie(state => state.trie);
   const {trackDocumentUpdate} = useAmplitude();
 
   const {mutate, isPending} = useMutation<PostDocumentContent, WikiDocument>({
     mutationFn: putDocumentClient,
     onSuccess: document => {
       trackDocumentUpdate(document.title, document.documentUUID);
+      // 문서 제목 업데이트 기능 추가 시 trie.update에 변경 전 문서 제목을 넣어야 합니다
+      trie.update(document.title, document.title, document.documentUUID);
       router.push(`${URLS.wiki}/${document.documentUUID}`);
       router.refresh();
     },
