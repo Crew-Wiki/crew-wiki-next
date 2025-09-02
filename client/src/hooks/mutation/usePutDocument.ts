b8321c57a002b98e260fac8731ec724fb7d79d2d
bmj0053 <bmj05378@gmail.com>
2025-09-02 16:24:34 +0900

    comment: 주석 TODO 추가

--- client/src/hooks/mutation/usePutDocument.ts
+++ client/src/hooks/mutation/usePutDocument.ts
@@ -17,7 +17,7 @@ export const usePutDocument = () => {
     mutationFn: putDocumentClient,
     onSuccess: document => {
       trackDocumentUpdate(document.title, document.documentUUID);
-      // 문서 제목 업데이트 기능 추가 시 updateTitle에 변경 전 문서 제목을 넣어야 합니다
+      // TODO: 문서 제목 업데이트 기능 추가 시 updateTitle에 변경 전 문서 제목을 넣어야 합니다
       updateTitle(document.title, document.title, document.documentUUID);
       router.push(`${URLS.wiki}/${document.documentUUID}`);
     },
