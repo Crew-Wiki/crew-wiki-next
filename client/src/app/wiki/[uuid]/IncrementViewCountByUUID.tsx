6ee64e8e76f948641a310a9ef9f473920623f7f4
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-09-14 22:50:52 +0900

    feat: 조회수 누적 기능 구현 (#118)
    * feat: 누적한 조회수를 백엔드로 전송하는 api 구현
    
    * feat: 특정 file을 상호배제로 접근할 수 있는 withLock 메서드 구현
    
    * feat: 조회수를 누적하는 기능 구현
    
    * chore: data 디렉터리 git으로 관리되지 않도록 설정
    
    * feat: node에서 file을 읽는 함수 구현
    
    * feat: 임계치가 되었을 때 백엔드로 조회수를 전송하는 기능 구현
    
    * refactor: ViewCountByUUID 타입 정의
    
    * feat: 조회수가 증가할 때 프론트엔드 서버에 누적해두었다가 임계치가 되었을 때 전송하는 기능 구현
    
    * style: console.log 제거
    
    * style: postViewsFlush 매개변수 이름 변경
    
    * style: flushViewCountIfNecessary 내부 combined 타입 정의된 것을 사용하도록 변경
    
    * style: view data file path를 환경변수 처리
    
    * feat: 조회수 증가 에러가 나도 사용자에게 에러 화면이 보이지 않도록 에러처리
    
    * refactor: postViewsFlush 요청에 실패했을 때만 두 번째 lock을 점유하도록 설정
    
    * feat: flush threshold 환경변수 처리
    
    * refactor: current, failed를 accumulative_count 하나로 관리해서 가독성 증가
    
    * fix: 프로세스 데드락 위험과, 재귀 제거로 안정성을 챙기는 방향으로 변경

---
+++ client/src/app/wiki/[uuid]/IncrementViewCountByUUID.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import {usePostViewCount} from '@hooks/mutation/usePostViewCount';
+import {useEffect} from 'react';
+
+type IncrementViewCountProps = {
+  uuid: string;
+};
+
+export const IncrementViewCountByUUID = ({uuid}: IncrementViewCountProps) => {
+  const {postViewCount} = usePostViewCount();
+
+  useEffect(() => {
+    if (uuid) postViewCount({uuid});
+  }, [uuid]);
+
+  return null;
+};
