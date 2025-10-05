25d0928dc6831cf763a16dca7e45f2b0bb900959
jinho_kim98 <rlawlsgh1227@gmail.com>
2025-10-05 23:08:52 +0900

    feat: 칩 사용예시 추가

--- client/src/app/wiki/statistics/page.tsx
+++ client/src/app/wiki/statistics/page.tsx
@@ -1,8 +1,11 @@
+import {Chip} from '@components/common/Chip';
+
 const Page = () => {
   return (
     <section className="flex w-full flex-col items-center gap-6">
       <div className="flex h-fit min-h-[864px] w-full flex-col gap-6 rounded-xl border border-solid border-primary-100 bg-white p-8 max-md:gap-2 max-md:p-4">
         <h1 className="font-bm text-2xl text-grayscale-800">테스트</h1>
+        <Chip text="크루위키부흥위원회" />
       </div>
     </section>
   );
