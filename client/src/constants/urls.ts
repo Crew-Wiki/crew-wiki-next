560be5a02caed2d31906292994454c521d490d3e
jinho_kim98 <rlawlsgh1227@gmail.com>
2025-10-07 19:56:18 +0900

    refactor: production, development, test 환경인지 체크하는 변수 추가

--- client/src/constants/urls.ts
+++ client/src/constants/urls.ts
@@ -1,13 +1,12 @@
+import {isProduction} from '@utils/nodeEnv';
+
 export const URLS = {
   main: '/',
   wiki: '/wiki',
   docs: ':title',
   edit: '/edit',
   post: '/post',
-  daemoon:
-    process.env.NODE_ENV === 'production'
-      ? '30a6c25d-4b88-11f0-99c4-0a270fc3fae1'
-      : '79febfcd-374f-419e-8138-966c6dbf5624',
+  daemoon: isProduction ? '30a6c25d-4b88-11f0-99c4-0a270fc3fae1' : '79febfcd-374f-419e-8138-966c6dbf5624',
   logs: 'logs',
   specificLog: 'log/:logId',
 };
