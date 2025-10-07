c247fde5e1a2db56a10eaa9ead18c4ffe806503e
jinho_kim98 <rlawlsgh1227@gmail.com>
2025-10-07 20:04:34 +0900

    refactor: Conflict Modal 컴포넌트 부분 분리

--- client/src/components/document/Write/useConflictModal.tsx
+++ client/src/components/document/Write/useConflictModal.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import Button from '@components/common/Button';
-import TuiEditor from '@components/document/TuiEditor';
 import {useEffect, useState} from 'react';
-import {Modal} from '@components/common/Modal/Modal';
 import {useModal} from '@components/common/Modal/useModal';
+import {ConflictModal} from './ConflictModal';
 
 interface ConflictModalProps {
   initialContent: string;
@@ -30,21 +28,13 @@ export const useConflictModal = ({initialContent, onResolve}: ConflictModalProps
   };
 
   const modal = useModal<boolean>(
-    <Modal>
-      <h2 className="mb-4 text-2xl font-bold">문서 충돌 해결</h2>
-      <p className="mb-4">다른 사용자가 문서를 수정했습니다. 아래 내용을 병합하여 저장해주세요.</p>
-      <div className="mb-4">
-        <TuiEditor initialValue={initialContent} onChange={setContent} />
-      </div>
-      <div className="flex justify-end gap-2">
-        <Button style="tertiary" size="m" onClick={() => modal.close()}>
-          취소
-        </Button>
-        <Button style="primary" size="m" onClick={handleResolve} disabled={!isResolved}>
-          충돌 해결 완료
-        </Button>
-      </div>
-    </Modal>,
+    <ConflictModal
+      initialContent={initialContent}
+      setContent={setContent}
+      closeModal={() => modal.close()}
+      handleResolve={handleResolve}
+      isResolved={isResolved}
+    />,
     {closeOnClickDimmedLayer: false},
   );
 
