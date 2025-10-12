f47e84ee7ff25629a17d69fa8b928c16913e3dad
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-10-12 16:42:57 +0900

    feat: dev, prod 각자의 대문 문서로 리다이렉트 (#139)
    * feat: dev, prod 환경에 따라 각자 대문 페이지를 보여주도록 설정
    
    * refactor: production, development, test 환경인지 체크하는 변수 추가
    
    * refactor: 대문 uuid 값 상수화

--- client/src/constants/urls.ts
+++ client/src/constants/urls.ts
@@ -1,10 +1,17 @@
+import {isProduction} from '../utils/nodeEnv';
+
+const DAEMOON = {
+  production: '30a6c25d-4b88-11f0-99c4-0a270fc3fae1',
+  development: '79febfcd-374f-419e-8138-966c6dbf5624',
+};
+
 export const URLS = {
   main: '/',
   wiki: '/wiki',
   docs: ':title',
   edit: '/edit',
   post: '/post',
-  daemoon: '30a6c25d-4b88-11f0-99c4-0a270fc3fae1',
+  daemoon: isProduction ? DAEMOON.production : DAEMOON.development,
   logs: 'logs',
   specificLog: 'log/:logId',
 };
