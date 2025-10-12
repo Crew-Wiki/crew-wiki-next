aedafa7619f2d961454af8c03da1915a16c1fa20
jinho_kim98 <rlawlsgh1227@gmail.com>
2025-10-12 16:34:06 +0900

    refactor: 대문 uuid 값 상수화

--- client/src/constants/urls.ts
+++ client/src/constants/urls.ts
@@ -1,4 +1,9 @@
-import {isProduction} from '@utils/nodeEnv';
+import {isProduction} from '../utils/nodeEnv';
+
+const DAEMOON = {
+  production: '30a6c25d-4b88-11f0-99c4-0a270fc3fae1',
+  development: '79febfcd-374f-419e-8138-966c6dbf5624',
+};
 
 export const URLS = {
   main: '/',
@@ -6,7 +11,7 @@ export const URLS = {
   docs: ':title',
   edit: '/edit',
   post: '/post',
-  daemoon: isProduction ? '30a6c25d-4b88-11f0-99c4-0a270fc3fae1' : '79febfcd-374f-419e-8138-966c6dbf5624',
+  daemoon: isProduction ? DAEMOON.production : DAEMOON.development,
   logs: 'logs',
   specificLog: 'log/:logId',
 };
