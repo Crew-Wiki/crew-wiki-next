a13d3bdbc3d04dec21bd28a438c8bc95da48d220
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-10-12 16:42:38 +0900

    feat: 칩 컴포넌트 구현 (#137)
    * feat: Chip 컴포넌트 구현
    
    * feat: 칩 사용예시 추가

---
+++ client/src/components/common/Chip/index.tsx
@@ -0,0 +1,29 @@
+import {ComponentProps} from 'react';
+
+type ChipProps = ComponentProps<'button'> & {
+  text: string;
+};
+
+export const Chip = ({text, ...buttonProps}: ChipProps) => {
+  return (
+    <button
+      type="button"
+      className="flex w-fit flex-row items-center gap-2 rounded-full border border-solid border-primary-300 bg-white px-3 py-1 active:bg-grayscale-100"
+      {...buttonProps}
+    >
+      <span className="font-pretendard text-sm font-normal text-primary-300">{text}</span>
+      <div className="w-4 border-none bg-transparent">
+        <svg
+          className="h-full w-full"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M5 5L19 19M19 5L5 19" stroke="#25B4B9" strokeWidth="2.2" strokeLinecap="round" />
+        </svg>
+      </div>
+    </button>
+  );
+};
