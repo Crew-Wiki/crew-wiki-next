ee3e05047567d513f10f4ff6565fab22a8618352
ShinjungOh <tlswjd9260@gmail.com>
2025-10-13 22:18:13 +0900

    feat: middleware 주석 해제

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -5,14 +5,13 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   const {pathname} = new URL(request.url);
 
-  // TODO: 개발 완료 후 주석 해제
-  // if (pathname === '/admin/login' && token) {
-  //   return NextResponse.redirect(new URL('/admin/documents', request.url));
-  // }
-  //
-  // if (pathname !== '/admin/login' && !token) {
-  //   return NextResponse.redirect(new URL('/admin/login', request.url));
-  // }
+  if (pathname === '/admin/login' && token) {
+    return NextResponse.redirect(new URL('/admin/documents', request.url));
+  }
+
+  if (pathname !== '/admin/login' && !token) {
+    return NextResponse.redirect(new URL('/admin/login', request.url));
+  }
 
   return NextResponse.next();
 }
