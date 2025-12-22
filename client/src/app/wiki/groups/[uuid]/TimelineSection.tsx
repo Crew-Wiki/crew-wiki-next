7164aa5a46797049838f330af56e3898fa32b3fb
ShinjungOh <tlswjd9260@gmail.com>
2025-12-22 13:20:31 +0900

    refactor: 새로고침 로직을 useRouter로 변경

--- client/src/app/wiki/groups/[uuid]/TimelineSection.tsx
+++ client/src/app/wiki/groups/[uuid]/TimelineSection.tsx
@@ -6,6 +6,7 @@ import {useModal} from '@components/common/Modal/useModal';
 import EventAddModal from '@components/group/EventAddModal';
 import {EventFormData} from '@type/Event.type';
 import {OrganizationEventResponse} from '@type/Group.type';
+import {useRouter} from 'next/navigation';
 
 // react-chrono 라이브러리 때문에 hydration 오류가 발생(라이브러리가 내부적으로 브라우저 전용 API를 사용해서 서버 렌더링 결과와 클라이언트 렌더링 결과가 다름)
 // Timeline 컴포넌트를 동적 import해서 SSR을 비활성화
@@ -24,6 +25,8 @@ interface TimelineSectionProps {
 }
 
 const TimelineSection = ({events, organizationDocumentUuid}: TimelineSectionProps) => {
+  const router = useRouter();
+
   const handleAddEvent = async (data: {date: Date; title: string; contents: string; writer: string}) => {
     const year = data.date.getFullYear();
     const month = String(data.date.getMonth() + 1).padStart(2, '0');
@@ -47,7 +50,7 @@ const TimelineSection = ({events, organizationDocumentUuid}: TimelineSectionProp
 
       if (response.ok) {
         closeModal();
-        window.location.reload();
+        router.refresh();
       } else {
         console.error('이벤트 추가 실패');
       }
