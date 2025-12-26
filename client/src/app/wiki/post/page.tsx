ed78f8924922191efee791959bdd30d0fa93eed3
ShinjungOh <tlswjd9260@gmail.com>
2025-12-27 00:45:28 +0900

    feat: 문서 작성 및 편집 페이지에 소속 컴포넌트 적용

--- client/src/app/wiki/post/page.tsx
+++ client/src/app/wiki/post/page.tsx
@@ -1,18 +1,38 @@
 'use client';
 
+import {useEffect, useState} from 'react';
 import PostHeader from '@components/document/Write/PostHeader';
 import TitleInputField from '@components/document/Write/TitleInputField';
-
+import OrganizationInputField from '@components/document/Write/OrganizationInputField';
 import TuiEditor from '@components/document/TuiEditor';
 import {usePostSaveMarkdown} from './usePostSaveMarkdown';
-import {useEffect} from 'react';
 import {useDocument} from '@store/document';
+import {Organization} from '@type/Group.type';
 
 const Page = () => {
   const {saveMarkdown, initialValue} = usePostSaveMarkdown();
   const setInit = useDocument(action => action.setInit);
   const reset = useDocument(action => action.reset);
   const onChange = useDocument(action => action.onChange);
+
+  const [selectedOrganizations, setSelectedOrganizations] = useState<Organization[]>([]);
+
+  const handleSelectOrganization = (organization: Organization) => {
+    setSelectedOrganizations(prev => [...prev, organization]);
+  };
+
+  const handleAddOrganization = (title: string) => {
+    const newOrganization: Organization = {
+      title,
+      uuid: crypto.randomUUID(),
+    };
+
+    setSelectedOrganizations(prev => [...prev, newOrganization]);
+  };
+
+  const handleRemoveOrganization = (uuid: string) => {
+    setSelectedOrganizations(prev => prev.filter(org => org.uuid !== uuid));
+  };
 
   useEffect(() => {
     setInit(
@@ -31,6 +51,12 @@ const Page = () => {
     <section className="flex h-fit w-full flex-col gap-6 rounded-xl border border-solid border-primary-100 bg-white p-8 max-[768px]:gap-3 max-[768px]:p-4">
       <PostHeader mode="post" />
       <TitleInputField />
+      <OrganizationInputField
+        selectedOrganizations={selectedOrganizations}
+        onSelect={handleSelectOrganization}
+        onAdd={handleAddOrganization}
+        onRemove={handleRemoveOrganization}
+      />
       <TuiEditor
         initialValue={initialValue}
         saveMarkdown={saveMarkdown}
