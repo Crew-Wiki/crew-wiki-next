1bf40ab7190ea8e40a3a6c2cdd1e1e689f37d113
ShinjungOh <tlswjd9260@gmail.com>
2025-12-27 00:45:28 +0900

    feat: 문서 작성 및 편집 페이지에 소속 컴포넌트 적용

--- client/src/app/wiki/[uuid]/edit/page.tsx
+++ client/src/app/wiki/[uuid]/edit/page.tsx
@@ -3,11 +3,13 @@
 import PostHeader from '@components/document/Write/PostHeader';
 import TitleInputField from '@components/document/Write/TitleInputField';
 import TuiEditor from '@components/document/TuiEditor';
+import OrganizationInputField from '@components/document/Write/OrganizationInputField';
 import {useParams} from 'next/navigation';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {useDocument} from '@store/document';
 import {LatestWikiDocument} from '@type/Document.type';
 import {useGetLatestDocumentByUUID} from '@hooks/fetch/useGetLatestDocumentByUUID';
+import {Organization} from '@type/Group.type';
 
 type EditPageProps = {
   document: LatestWikiDocument;
@@ -17,6 +19,25 @@ const EditPage = ({document}: EditPageProps) => {
   const setInit = useDocument(action => action.setInit);
   const reset = useDocument(action => action.reset);
   const onChange = useDocument(action => action.onChange);
+
+  const [selectedOrganizations, setSelectedOrganizations] = useState<Organization[]>([]);
+
+  const handleSelectOrganization = (organization: Organization) => {
+    setSelectedOrganizations(prev => [...prev, organization]);
+  };
+
+  const handleAddOrganization = (title: string) => {
+    const newOrganization: Organization = {
+      title,
+      uuid: crypto.randomUUID(),
+    };
+
+    setSelectedOrganizations(prev => [...prev, newOrganization]);
+  };
+
+  const handleRemoveOrganization = (uuid: string) => {
+    setSelectedOrganizations(prev => prev.filter(org => org.uuid !== uuid));
+  };
 
   useEffect(() => {
     setInit(
@@ -36,6 +57,12 @@ const EditPage = ({document}: EditPageProps) => {
     <section className="flex h-fit w-full flex-col gap-6 rounded-xl border border-solid border-primary-100 bg-white p-8 max-[768px]:gap-3 max-[768px]:p-4">
       <PostHeader mode="edit" />
       <TitleInputField />
+      <OrganizationInputField
+        selectedOrganizations={selectedOrganizations}
+        onSelect={handleSelectOrganization}
+        onAdd={handleAddOrganization}
+        onRemove={handleRemoveOrganization}
+      />
       <TuiEditor initialValue={document.contents} onChange={value => onChange(value, 'contents')} />
     </section>
   );
