5842f51fa22e5a7ab61eb8125dad65df36fd5a4d
ShinjungOh <tlswjd9260@gmail.com>
2026-01-04 00:09:10 +0900

    refactor: 날짜 비교 로직 유틸 함수 적용

--- client/src/utils/date.ts
+++ client/src/utils/date.ts
@@ -13,3 +13,11 @@ export const formatDateDashed = (date: Date): string => {
   const {year, month, day} = extractDate(date);
   return `${year}-${month}-${day}`;
 };
+
+export const isSameDate = (date1: Date, date2: Date): boolean => {
+  return (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate()
+  );
+}
