4a0a8a8dc9bd1289f602333771b0090874b26815
ShinjungOh <tlswjd9260@gmail.com>
2026-01-05 21:20:22 +0900

    refactor: Layout Shift를 방지하기 위한 로딩 화면 width 조정

--- client/src/app/wiki/groups/[uuid]/loading.tsx
+++ client/src/app/wiki/groups/[uuid]/loading.tsx
@@ -1,7 +1,9 @@
 const Loading = () => {
   return (
-    <div className="flex h-96 items-center justify-center">
-      <div className="text-grayscale-lightText">그룹 정보를 불러오는 중...</div>
+    <div className="flex w-full flex-col gap-6 max-[768px]:gap-2">
+      <section className="flex h-fit min-h-[864px] w-full flex-col items-center justify-center rounded-xl border border-solid border-primary-100 bg-white p-8 max-md:p-4">
+        <div className="text-grayscale-lightText">그룹 정보를 불러오는 중...</div>
+      </section>
     </div>
   );
 };
