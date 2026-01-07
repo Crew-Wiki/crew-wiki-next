f6619fc1568a3eaa06ea570f3419f95ccaccb899
ShinjungOh <tlswjd9260@gmail.com>
2026-01-08 05:23:31 +0900

    feat: 조직 문서 편집 기능 구현 - 조직 문서 편집 페이지 추가 - 조직 문서 조회/수정 API 추가

--- client/src/app/wiki/groups/[uuid]/page.tsx
+++ client/src/app/wiki/groups/[uuid]/page.tsx
@@ -24,9 +24,13 @@ const GroupPage = async ({params}: {params: Promise<{uuid: string}>}) => {
 
   return (
     <div className="flex w-full flex-col gap-6 max-[768px]:gap-2">
-      <MobileDocumentHeader uuid={groupDocument.organizationDocumentUuid} />
+      <MobileDocumentHeader uuid={groupDocument.organizationDocumentUuid} documentType="ORGANIZATION" />
       <section className="flex h-fit min-h-[864px] w-full flex-col gap-6 rounded-xl border border-solid border-primary-100 bg-white p-8 max-md:gap-2 max-md:p-4 max-[768px]:gap-2">
-        <DocumentHeader title={groupDocument.title} uuid={groupDocument.organizationDocumentUuid} />
+        <DocumentHeader
+          title={groupDocument.title}
+          uuid={groupDocument.organizationDocumentUuid}
+          documentType="ORGANIZATION"
+        />
         <TOC headTags={extractHeadings(htmlContents)} />
         <div className="toastui-editor-contents" dangerouslySetInnerHTML={{__html: htmlContents}} />
 
--- client/src/components/document/layout/MobileDocumentHeader.tsx
+++ client/src/components/document/layout/MobileDocumentHeader.tsx
@@ -4,22 +4,28 @@ import Link from 'next/link';
 
 interface MobileDocumentHeaderProps {
   uuid: string;
+  documentType?: 'CREW' | 'ORGANIZATION';
 }
 
-const MobileDocumentHeader = ({uuid}: MobileDocumentHeaderProps) => {
+const MobileDocumentHeader = ({uuid, documentType = 'CREW'}: MobileDocumentHeaderProps) => {
+  const editHref = documentType === 'ORGANIZATION' ? route.goWikiGroupEdit(uuid) : route.goWikiEdit(uuid);
+
   return (
     <div className="md:hidden">
       <fieldset className="flex gap-2 max-md:w-full max-md:justify-center">
-        <Link href={route.goWikiEdit(uuid)}>
+        <Link href={editHref}>
           <Button style="tertiary" size="xs">
             편집하기
           </Button>
         </Link>
-        <Link href={route.goWikiLogs(uuid)}>
-          <Button style="tertiary" size="xs">
-            편집로그
-          </Button>
-        </Link>
+        {/* TODO: 조직문서에도 편집로그 기능 추가되면 수정 필요 */}
+        {documentType === 'CREW' && (
+          <Link href={route.goWikiLogs(uuid)}>
+            <Button style="tertiary" size="xs">
+              편집로그
+            </Button>
+          </Link>
+        )}
         <Link href={route.goWikiWrite()}>
           <Button style="primary" size="xs">
             작성하기
