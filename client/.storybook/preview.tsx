1956fd0a32cd913539682a40aa7c742b6fac4766
ShinjungOh <tlswjd9260@gmail.com>
2026-02-05 12:21:39 +0900

    chore: 스토리 중앙 정렬 설정

--- client/.storybook/preview.tsx
+++ client/.storybook/preview.tsx
@@ -3,6 +3,7 @@ import '../src/app/globals.css';
 
 export default definePreview({
   parameters: {
+    layout: 'centered',
     controls: {
       matchers: {
         color: /(background|color)$/i,
