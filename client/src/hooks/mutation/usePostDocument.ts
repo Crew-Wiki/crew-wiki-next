4991f06e9e285f05d629973dbd4566c86818cc84
Luna <97431021+ShinjungOh@users.noreply.github.com>
2026-02-25 15:07:05 +0900

    fix: 문서 편집 후 캐시 반영 및 제목 공백 중복 검사 개선 (#199)
    * fix: 문서 제목 중복 검사 시 앞뒤 공백 무시하도록 trim 처리
    
    * fix: 문서 편집 후 캐시가 즉시 반영되지 않는 문제 해결

--- client/src/hooks/mutation/usePostDocument.ts
+++ client/src/hooks/mutation/usePostDocument.ts
@@ -2,14 +2,12 @@
 
 import useMutation from '@hooks/useMutation';
 import {PostDocumentContent, WikiDocument} from '@type/Document.type';
-import {useRouter} from 'next/navigation';
 import useAmplitude from '@hooks/useAmplitude';
 import {postDocumentClient} from '@apis/client/document';
 import {useTrie} from '@store/trie';
 import {route} from '@constants/route';
 
 export const usePostDocument = () => {
-  const router = useRouter();
   const addTitle = useTrie(state => state.addTitle);
   const {trackDocumentCreate} = useAmplitude();
 
@@ -18,8 +16,7 @@ export const usePostDocument = () => {
     onSuccess: document => {
       trackDocumentCreate(document.title, document.documentUUID);
       addTitle(document.title, document.documentUUID);
-      router.push(route.goWiki(document.documentUUID));
-      router.refresh();
+      window.location.href = route.goWiki(document.documentUUID);
     },
   });
 
--- client/src/hooks/mutation/usePutDocument.ts
+++ client/src/hooks/mutation/usePutDocument.ts
@@ -2,14 +2,12 @@
 
 import useMutation from '@hooks/useMutation';
 import {PostDocumentContent, WikiDocument} from '@type/Document.type';
-import {useRouter} from 'next/navigation';
 import useAmplitude from '@hooks/useAmplitude';
 import {putDocumentClient} from '@apis/client/document';
 import {useTrie} from '@store/trie';
 import {route} from '@constants/route';
 
 export const usePutDocument = () => {
-  const router = useRouter();
   const updateTitle = useTrie(state => state.updateTitle);
   const {trackDocumentUpdate} = useAmplitude();
 
@@ -19,8 +17,7 @@ export const usePutDocument = () => {
       trackDocumentUpdate(document.title, document.documentUUID);
       // TODO: 문서 제목 업데이트 기능 추가 시 updateTitle에 변경 전 문서 제목을 넣어야 합니다
       updateTitle(document.title, document.title, document.documentUUID);
-      router.push(route.goWiki(document.documentUUID));
-      router.refresh();
+      window.location.href = route.goWiki(document.documentUUID);
     },
   });
 
