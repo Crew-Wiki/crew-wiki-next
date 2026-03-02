0cd2415b8d5701a46dc8607ccfca954b796e974b
jinhokim98 <rlawlsgh1227@gmail.com>
2026-03-02 19:40:39 +0900

    fix: z index 변경

--- client/src/app/wiki/loading.tsx
+++ client/src/app/wiki/loading.tsx
@@ -2,7 +2,7 @@ import {LoadingSpinner} from '@components/common/LoadingSpinner';
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/90 transition-opacity duration-200">
+    <div className="fixed inset-0 z-30 flex items-center justify-center bg-white/90 transition-opacity duration-200">
       <LoadingSpinner size="m" thickness="thick" />
     </div>
   );
